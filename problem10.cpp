2317dbb01f8d6a632ef6e14514f0c4d342833359
TorbenGit <torben-ebermann@web.de>
2022-03-08 20:54:56 +0100

    Optimized Problem 10

--- problem10.cpp
+++ problem10.cpp
@@ -2,60 +2,47 @@
 #include <vector>
 #include <thread>
 #include <future>
+#include <math.h>
 
-void prime_sieve_thread(std::vector<int> v, std::promise<std::vector<int>> && ret) {
-    int start = v.front();
-    int end = v.back();
-
-    std::cout << "Starting Thread " << start << " - " << end << "\n";
+void prime_sieve_thread(unsigned int id, std::vector<int> v, std::promise<std::vector<int>> && ret) {
+    std::cout << "Starting Thread " << id << "\n";
 
     for (size_t i=0; i<v.size(); i++) {
-        for (int j=2; j<v[i]; j++) {
-            if (v[i] != 0 && !(v[i]%j)) {
+        for (int j=2; j<=std::ceil(std::sqrt(v[i])); j++) {
+            if (!(v[i]%j) && j != v[i]) {
                 v[i] = 0;
                 break;
             }
         }
     }
 
-    std::cout << "Finished Thread " << start << " - " << end << "\n";
+    std::cout << "Finished Thread " << id << "\n";
 
     ret.set_value(v);
 }
 
-std::vector<int> primeSieve(int n) {
+std::vector<int> prime_sieve(unsigned int n) {
     const auto proc_count = std::thread::hardware_concurrency();
-    std::vector<int> primes[proc_count];
-
-    int remaining = n;
-    int before = 2;
-    int d = std::div(remaining,proc_count).quot;
-
     std::pair<std::thread, std::future<std::vector<int>>> threads[proc_count];
 
+    std::vector<int> vec_arr[proc_count];
+
     for (unsigned int i=0; i<proc_count; i++) {
-        std::vector<int> v;
+        vec_arr[i] = std::vector<int>();
+    }
 
-        if (i != proc_count-1) {
-            int end = d*(i+1);
-
-            for (int j=before; j<end; j++) {
-                v.push_back(j);
-            }
-
-            remaining -= (end-before);
-            before = end;
-        } else {
-            int end = n;
-
-            for (int j=before; j<=end; j++) {
-                v.push_back(j);
-            }
+    for (unsigned int i=2, pc=0; i<=n; i++, pc++) {
+        if (pc == proc_count) {
+            pc = 0;
         }
 
+        vec_arr[pc].push_back(i);
+    }
+
+    for (unsigned int i=0; i<proc_count; i++) {
         std::promise<std::vector<int>> vp;
         auto f = vp.get_future();
-        std::thread t(prime_sieve_thread, v, std::move(vp));
+        std::thread t(prime_sieve_thread, i+1, vec_arr[i], std::move(vp));
 
         std::pair<std::thread, std::future<std::vector<int>>> p;
         p.first = std::move(t);
@@ -63,7 +50,7 @@ std::vector<int> primeSieve(int n) {
         threads[i] = std::move(p);
     }
 
-    std::vector<int> filteredPrimes;
+    std::vector<int> filtered_primes;
 
     for (size_t i=0; i<proc_count; i++) {
         threads[i].first.join();
@@ -71,18 +58,18 @@ std::vector<int> primeSieve(int n) {
 
         for (size_t j=0; j<v.size(); j++) {
             if (v[j] != 0) {
-                filteredPrimes.push_back(v[j]);
+                filtered_primes.push_back(v[j]);
             }
         }
     }
 
-    return filteredPrimes;
+    return filtered_primes;
 }
 
 int main() {
-    std::vector<int> p = primeSieve(2000000-1);
+    std::vector<int> p = prime_sieve(2000000-1);
 
-    long long int sum = 0;
+    long int sum = 0;
 
     for (size_t i=0; i<p.size(); i++) {
         sum += p[i];
